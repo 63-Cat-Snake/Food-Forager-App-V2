cf1ec519c351fbc24318a45121f1b97991ef1fea
QuanN <jeanquan98@gmail.com>
2024-02-22 14:16:14 -0600

    QuanN 2/22 Quan-Nick-BackEnd Updated some tests
    Co-author-by: Nick <kravchuknick@gmail.com>

--- __tests__/servertest.js
+++ __tests__/servertest.js
@@ -5,35 +5,54 @@ const mongoose = require('mongoose');
 const MONGO_URI =
   'mongodb+srv://kravchuknick:jDnS1N7Xt0kPSBP8@food-forager-app-v2.jzse2jb.mongodb.net/';
 
-// const server = 'http://localhost:3000';
-// const request = supertest(app);
-
 describe('Route integration unit test', () => {
   /* Connecting to the database before each test. */
-  beforeEach(async () => {
+  beforeAll(async () => {
     await mongoose.connect(MONGO_URI);
   });
 
   /* Closing database connection after each test. */
-  afterEach(async () => {
+  afterAll(async () => {
     await mongoose.connection.close();
   });
 
-  // GET request to ROOT
   describe('Root endpoint', () => {
     it('should respond with HTML file and status 200', async () => {
       const res = await request(app).get('/');
       expect(res.statusCode).toBe(200);
       expect(res.header['content-type']).toBe('text/html; charset=UTF-8');
     });
+    // TODO: check if MainDisplay recieve the last recent API calls
+  });
+
+  describe('Restaurants endpoint', () => {
+    it('should respond with JSON format and status 200', async () => {
+      const res = await request(app).get('/restaurants');
+      expect(res.statusCode).toBe(200);
+      expect(res.header['content-type']).toBe(
+        'application/json; charset=utf-8'
+      );
+    });
+
+    it('should be in Array form', async () => {
+      const res = await request(app).get('/restaurants');
+      expect(res.body).toBeInstanceOf(Array);
+    });
+  });
+
+  xdescribe('Favorite endpoint', () => {
+    it('should respond with JSON format and status 200', async () => {
+      const res = await request(app).patch('/favorite').send({});
+      expect(res.statusCode).toBe(200);
+      expect(res.header['content-type']).toBe(
+        'application/json; charset=utf-8'
+      );
+    });
+
+    it('should be in Object form', async () => {
+      const res = await request(app).patch('/favorite').send({});
+      expect(res.body).toBeInstanceOf(Object);
+      // expect(res.body).toHaveProperty('favorite');
+    });
   });
 });
-
-/*
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
-
-*/
