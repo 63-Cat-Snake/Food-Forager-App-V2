4e80cf34a0fa9b302622b06ea28b6eda144fa4d9
QuanN <jeanquan98@gmail.com>
2024-02-22 19:07:53 -0600

    QuanN 2/22 dev Fixed little misc & tried to update fav stretch feature dquote> dquote> dquote> Co-author-by: Justin <hozion612356@gmail.com> dquote> Co-author-by: Andy <anwmail@skiff.com> dquote> Co-author-by: Rob <robsin@gmail.com> dquote> Co-author-by: Nick <kravchuknick@gmail.com>”

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -2,11 +2,11 @@ const express = require('express');
 const apiController = {};
 const axios = require('axios');
 const Restaurant = require('../models/restaurantModel');
+const Favorite = require('../models/favoriteModel');
 
 const APIKey = '1e971a009f6e4ab1bd6aa263960bd053';
 
 apiController.favorite = async (req, res, next) => {
-  console.log('req.body is ', req.body);
   let { cuisine, budget, distance, latitude, longitude, _id } = req.body;
 
   latitude = Math.floor(latitude);
@@ -21,11 +21,41 @@ apiController.favorite = async (req, res, next) => {
       longitude,
     });
 
+    // Get an array with all Favorite IDS
+    const favorite = await Favorite.find();
+
+    // JUST IN CASE OUR COLLECTION IS EMPTY
+    if (favorite.length === 0) {
+      // if nothing inside favorite model
+      const favCreate = new Favorite({
+        favList: [_id],
+      });
+      await favCreate.save();
+    }
+
+    let newList;
     // Loop through and update favoriate TRUE OR FALSE
     result.restaurantList.forEach((restaurant) => {
       if (restaurant._id === _id)
-        restaurant.favorite = restaurant.favorite ? false : true;
+        if (!restaurant.favorite) {
+          restaurant.favorite = true; // mark as true
+          // push in the fav id if NOT INCLUDED
+          if (!favorite[0].favList.includes(_id)) favorite[0].favList.push(_id);
+        } else {
+          restaurant.favorite = false;
+          const index = favorite[0].favList.indexOf(_id);
+          // cut out the fav ID
+          newList = favorite[0].favList
+            .slice(0, index)
+            .concat(favorite[0].favList.slice(index + 1));
+        }
     });
+
+    // Update Favorite List
+    await Favorite.findOneAndUpdate(
+      { favList: favorite[0].favList },
+      { favList: newList }
+    );
 
     // Need to update the list in DATABASE after changing the value
     await Restaurant.findOneAndUpdate(
@@ -102,8 +132,6 @@ apiController.formatRequestData = async (req, res, next) => {
 
     // restaurantData will be an array of restaurant objects
     console.log(apiResponseData);
-    res.locals.restaurantData = apiResponseData.data.restaurants;
-
     const restaurantList = apiResponseData.data.restaurants;
 
     // Add Favorite to each restaurant object
@@ -117,8 +145,11 @@ apiController.formatRequestData = async (req, res, next) => {
       budget,
       distance,
     });
-
     await restaurant.save();
+
+    // Send list back
+    res.locals.restaurantData = apiResponseData.data.restaurants;
+
     return next();
   } catch (error) {
     console.error('Error fetching data from API:', error);
@@ -126,5 +157,3 @@ apiController.formatRequestData = async (req, res, next) => {
   }
 };
 module.exports = apiController;
-
-// 34.4358912 & longitude: -119.6228608
